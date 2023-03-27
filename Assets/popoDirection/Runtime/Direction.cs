5ccdb4baa7e329c189098f1b4f3434af82db3276
KYapp <kukiyomenaisapp@gmail.com>
2023-03-27 18:30:57 +0900

    vector2をDirectionに変換する関数を追加

--- Assets/popoDirection/Runtime/Direction.cs
+++ Assets/popoDirection/Runtime/Direction.cs
@@ -15,5 +15,21 @@ namespace JuhaKurisu.PopoTools.DirectionUtility
         public readonly static UpDirection upDirection = new UpDirection();
         public readonly static DownDirection downDirection = new DownDirection();
         public readonly static NoDirection noDirection = new NoDirection();
+
+        public static Direction ToDirection4(Vector2 vec, bool y = true)
+        {
+            if (vec == Vector2.zero) return noDirection;
+
+            if (y)
+            {
+                if (vec.y > 0) return upDirection;
+                else return downDirection;
+            }
+            else
+            {
+                if (vec.x > 0) return rightDirection;
+                else return leftDirection;
+            }
+        }
     }
 }
